a6f13fda6b404fd67f5233aed05dd1e8133aa956
Ilya Dragonromeo <pingvinromeo@gmail.com>
2022-03-10 21:22:26 +0300

    feat:realses vers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-module.exports = function toReadable(number) {
-    // function toReadable(number) {
+// module.exports = function toReadable(number) {
+function toReadable(number) {
     let arrayHundred = ['zero', 'one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred', 'ten hundred'];
     let arr2 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
-    let arr3 = ['null1', 'null2', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let arr3 = ['', 'null2', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
     if (number <= 20) {
         return arr2[number];
@@ -18,19 +18,22 @@ module.exports = function toReadable(number) {
         let arrayOfThird = ("" + number).split("").map(Number)
         let firstNumb2 = arrayOfThird[0];
         console.log('первая переменная равна ' + firstNumb2)
-        let secondNumb2 = arrayOfThird[1];;
+        let secondNumb2 = arrayOfThird[1];
         console.log('вторая переменная равна ' + secondNumb2)
-        let thirdNumb2 = arrayOfThird[2];;
+        let thirdNumb2 = arrayOfThird[2];
         console.log('третья переменная равна ' + thirdNumb2)
-        return arrayHundred[firstNumb2] + ' ' + arr3[secondNumb2] + ' ' + arr2[thirdNumb2];
+            // let secondNumb3 = (arrayOfThird[1] + arrayOfThird[2]).join('');
+            // console.log('Тест складывания двух массивов' + secondNumb3);
+        if (arrayOfThird[1] > 2) {
+            return arrayHundred[firstNumb2] + ' ' + arr3[secondNumb2] + ' ' + arr2[thirdNumb2];
+        } else {
+            let firstNumb3 = arrayOfThird[0];
+            delete arrayOfThird[0];
+            let secondNumb3 = arrayOfThird.join('');
+            console.log('secondNumb3 равен ' + secondNumb3);
+            return arrayHundred[firstNumb2] + ' ' + arr2[secondNumb3];
+        }
+
     }
 }
-console.log(toReadable(865));
-
-
-// function divisionOfNumbers(number) {
-//     let array = ("" + number).split("").map(Number);
-//     return array;
-
-// }
-// console.log(divisionOfNumbers(123));
+console.log(toReadable(835));
